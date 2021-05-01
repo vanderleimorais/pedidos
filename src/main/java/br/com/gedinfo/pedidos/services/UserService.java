ba977e1de38b9d7ad2d6caddf61424293e3048ad
Vanderlei Morais <vanderlei@gedinfo.com.br>
2021-05-01 12:55:11 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/gedinfo/pedidos/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.gedinfo.pedidos.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.gedinfo.pedidos.entities.User;
+import br.com.gedinfo.pedidos.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
