bfd66b62f042a0e5fab828dae481dfc4d205c911
Vanderlei Morais <vanderlei@gedinfo.com.br>
2021-05-01 13:47:51 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/br/com/gedinfo/pedidos/services/OrderService.java
@@ -0,0 +1,26 @@
+package br.com.gedinfo.pedidos.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.gedinfo.pedidos.entities.Order;
+import br.com.gedinfo.pedidos.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
